a9e1d2b79c78cf22200a88d40dedfc707678d89f
heinricov abraham <heinricov@heinricovs-MacBook-Air.local>
2024-10-14 12:55:33 +0700

    add: logo

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,10 @@
 "use client";
 
-import { useState, useEffect } from 'react';
-import Link from 'next/link';
-import { usePathname } from 'next/navigation';
-import { Navbar, DarkThemeToggle, TextInput } from 'flowbite-react';
-import Image from 'next/image';
+import { useState, useEffect } from "react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { Navbar, DarkThemeToggle, TextInput } from "flowbite-react";
+import Image from "next/image";
 
 interface NavLinkProps {
   href: string;
@@ -26,9 +26,11 @@ export default function CustomNavbar() {
   const NavLink: React.FC<NavLinkProps> = ({ href, children }) => {
     const isActive = pathname === href;
     return (
-      <Link 
-        href={href} 
-        className={`${isActive ? 'text-blue-600 dark:text-white' : 'text-gray-500'} hover:text-white transition-colors`}
+      <Link
+        href={href}
+        className={`${
+          isActive ? "text-blue-600 dark:text-white" : "text-gray-500"
+        } hover:text-white transition-colors`}
       >
         {children}
       </Link>
@@ -37,12 +39,20 @@ export default function CustomNavbar() {
 
   return (
     <Navbar className="fixed w-full z-20 top-0 p-4 left-0 border-b border-gray-200 dark:border-gray-600">
-      <div className='flex justify-between'>
-        <Navbar.Brand as={Link} href="/" className='lg:mr-[10rem]'>
-          <Image src="./logo/logo-white.jpg" alt="logo" width={30} height={30} className="rounded-sm" />
-          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white ml-2">BagScript</span>
+      <div className="flex justify-between">
+        <Navbar.Brand as={Link} href="/" className="lg:mr-[10rem]">
+          <Image
+            src="./logo/logo-white.png"
+            alt="logo"
+            width={30}
+            height={30}
+            className="rounded-sm"
+          />
+          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white ml-2">
+            BagScript
+          </span>
         </Navbar.Brand>
-        <div className='lg:flex hidden justyfy-between gap-5 items-center'>
+        <div className="lg:flex hidden justyfy-between gap-5 items-center">
           <NavLink href="/">Home</NavLink>
           <NavLink href="/blogs">Blogs</NavLink>
           <NavLink href="/docs">Docs</NavLink>
@@ -57,9 +67,9 @@ export default function CustomNavbar() {
           className="mr-1 p-0 hidden lg:inline"
         />
         <DarkThemeToggle />
-        <Navbar.Toggle className='bg-transparent focus:bg-transparent' />
+        <Navbar.Toggle className="bg-transparent focus:bg-transparent" />
       </div>
-      <Navbar.Collapse className='lg:hidden'>
+      <Navbar.Collapse className="lg:hidden">
         <NavLink href="/">Home</NavLink>
         <NavLink href="/blogs">Blogs</NavLink>
         <NavLink href="/docs">Docs</NavLink>
